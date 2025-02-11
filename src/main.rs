814f9af87063cd4bcf47fce669a82d80e56efa6d
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-02-10 22:17:01 -0700

    feat: add and_compose pipe combinator for piping into other systems and composing effects

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,24 @@ where
     out
 }
 
+fn and_compose<E1, O1, System, Marker, E2, O2, E3>(
+    mut s: System,
+    compose_fn: impl Fn(E1, E2) -> E3,
+) -> impl FnMut(In<EffectOut<E1, O1>>, StaticSystemParam<System::Param>) -> EffectOut<E3, O2>
+where
+    System: SystemParamFunction<Marker, Out = EffectOut<E2, O2>>,
+    E1: Effect,
+    E2: Effect,
+    E3: Effect,
+    for<'a> System::In: SystemInput<Inner<'a> = O1>,
+{
+    move |In(EffectOut(e1, input)), params| {
+        let EffectOut(e2, out) = s.run(input, params.into_inner());
+
+        EffectOut(compose_fn(e1, e2), out)
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
