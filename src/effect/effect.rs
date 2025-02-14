b51bd598f4e562267188135a52e6fd7cad058b72
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-02-14 01:09:53 -0700

    docs: document effect module

--- src/effect/effect.rs
+++ src/effect/effect.rs
@@ -1,8 +1,15 @@
 use bevy::{ecs::system::SystemParam, prelude::*, utils::all_tuples};
 
+/// Define a state transition in `bevy`'s ECS.
+///
+/// Can be returned by `bevy` systems and `pipe`d into [`affect`] to perform the transition.
+///
+/// [`affect`]: crate::system_combinators::affect
 pub trait Effect {
+    /// The `SystemParam` this effect mutates.
     type MutParam: SystemParam;
 
+    /// Perform the state transition on the parameter.
     fn affect(self, param: &mut <Self::MutParam as SystemParam>::Item<'_, '_>);
 }
 
@@ -22,6 +29,10 @@ macro_rules! impl_effect {
 
 all_tuples!(impl_effect, 1, 8, E, e, p);
 
+/// An [`Effect`] and an output.
+///
+/// Can be returned by `bevy` systems to produce effects with `E` while preserving normal pipe
+/// functionality with `O`.
 pub struct EffectOut<E, O>(pub E, pub O)
 where
     E: Effect;
