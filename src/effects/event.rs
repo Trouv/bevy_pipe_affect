5d48cc676775009bf960f333ccfbd3bfdc3bc871
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-02-16 09:15:56 -0700

    test: add test for EventSend

--- src/effects/event.rs
+++ src/effects/event.rs
@@ -18,3 +18,38 @@ where
         param.send(self.0);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+    use proptest_derive::Arbitrary;
+
+    use super::*;
+    use crate::prelude::affect;
+
+    #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Event, Arbitrary)]
+    struct NumberEvent(u128);
+
+    proptest! {
+        #[test]
+        fn event_send_produces_events(events in prop::collection::vec(any::<NumberEvent>(), 1..10)) {
+            let mut app = App::new();
+
+            let mut events_clone = events.clone();
+            app.add_event::<NumberEvent>()
+                .add_systems(Update, (move || EventSend(events_clone.remove(0))).pipe(affect));
+
+            for expected in events {
+                app.update();
+
+                let events_in_update = app.world().resource::<Events<NumberEvent>>().iter_current_update_events().collect::<Vec<_>>();
+
+                prop_assert_eq!(events_in_update.len(), 1);
+
+                let event_sent = events_in_update.first().unwrap();
+
+                prop_assert_eq!(event_sent, &&expected);
+            }
+        }
+    }
+}
