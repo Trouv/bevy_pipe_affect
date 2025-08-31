a5700d66de411b796b7bd866b4c833405ee9988a
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-08-30 22:50:32 -0600

    feat: add `-Resource` related `Command` effects (#14)
    While the existing resource commands allow for resource modification,
    they don't allow for resource insertion/removal. This is typically done
    with commands, and deferred to the exclusive `apply_deferred` system.
    This adds `CommandInsertResource` and `CommandRemoveResource`
    accordingly.

--- src/effects/command.rs
+++ src/effects/command.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use bevy::prelude::*;
 
 use crate::Effect;
@@ -20,12 +22,63 @@ where
     }
 }
 
+/// [`Effect`] that queues a command for inserting the provided `Resource` in the `World`.
+#[doc = include_str!("defer_command_note.md")]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct CommandInsertResource<R>(pub R)
+where
+    R: Resource;
+
+impl<R> Effect for CommandInsertResource<R>
+where
+    R: Resource,
+{
+    type MutParam = Commands<'static, 'static>;
+
+    fn affect(self, param: &mut <Self::MutParam as bevy::ecs::system::SystemParam>::Item<'_, '_>) {
+        param.insert_resource(self.0);
+    }
+}
+
+/// [`Effect`] that queues a command for removing a `Resource` from the `World`.
+#[doc = include_str!("defer_command_note.md")]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct CommandRemoveResource<R>
+where
+    R: Resource,
+{
+    resource: PhantomData<R>,
+}
+
+impl<R> CommandRemoveResource<R>
+where
+    R: Resource,
+{
+    /// Construct a new [`CommandRemoveResource`]
+    pub fn new() -> Self {
+        CommandRemoveResource {
+            resource: PhantomData,
+        }
+    }
+}
+
+impl<R> Effect for CommandRemoveResource<R>
+where
+    R: Resource,
+{
+    type MutParam = Commands<'static, 'static>;
+
+    fn affect(self, param: &mut <Self::MutParam as bevy::ecs::system::SystemParam>::Item<'_, '_>) {
+        param.remove_resource::<R>();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use proptest::prelude::*;
 
     use super::*;
-    use crate::effects::number_data::NumberComponent;
+    use crate::effects::number_data::{NumberComponent, NumberResource};
     use crate::prelude::affect;
 
     proptest! {
@@ -62,6 +115,34 @@ mod tests {
             let component_count = app.world_mut().query_filtered::<(), With<NumberComponent<0>>>().iter(app.world()).count();
 
             assert_eq!(component_count, 2);
+        }
+
+        #[test]
+        fn resource_commands_correctly_insert_and_remove(resource in any::<NumberResource>()) {
+            let mut app = App::new();
+
+            assert!(app.world().get_resource::<NumberResource>().is_none());
+
+            #[derive(Debug, Clone, PartialEq, Eq, Hash, SystemSet)]
+            struct InsertSystem;
+
+            app.add_systems(
+                Update,
+                (move || CommandInsertResource(resource)).pipe(affect).in_set(InsertSystem),
+            );
+
+            app.update();
+
+            assert_eq!(app.world().get_resource::<NumberResource>(), Some(&resource));
+
+            app.add_systems(
+                Update,
+                (move || CommandRemoveResource::<NumberResource>::new()).pipe(affect).after(InsertSystem),
+            );
+
+            app.update();
+
+            assert!(app.world().get_resource::<NumberResource>().is_none());
         }
     }
 }
