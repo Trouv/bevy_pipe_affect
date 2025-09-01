810b45c702c28df7fa980e3b6abfa1a67e404363
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-08-31 20:06:32 -0600

    refactor: move effect impl on tuples to its own module (#16)
    The implementation of `Effect` for tuples is currently in the same
    module as the `Effect` definition. This breaks the current code
    organization patterns. This change moves it to its own module
    accordingly.

--- src/effect.rs
+++ src/effect.rs
@@ -1,6 +1,4 @@
 use bevy::ecs::system::SystemParam;
-use bevy::prelude::*;
-use variadics_please::all_tuples;
 
 /// Define a state transition in `bevy`'s ECS.
 ///
@@ -14,19 +12,3 @@ pub trait Effect {
     /// Perform the state transition on the parameter.
     fn affect(self, param: &mut <Self::MutParam as SystemParam>::Item<'_, '_>);
 }
-
-macro_rules! impl_effect {
-    ($(($E:ident, $e:ident, $p:ident)),*) => {
-        impl<$($E),*> Effect for ($($E,)*)
-        where $($E: Effect,)* {
-            type MutParam = ParamSet<'static, 'static, ($(<$E as Effect>::MutParam,)*)>;
-
-            fn affect(self, param: &mut <Self::MutParam as SystemParam>::Item<'_, '_>) {
-                let ($($e,)*) = self;
-                $($e.affect(&mut param.$p());)*
-            }
-        }
-    };
-}
-
-all_tuples!(impl_effect, 1, 8, E, e, p);
