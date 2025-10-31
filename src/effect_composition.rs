b66759fc1cf2f676050696545c25f0eb3a7770e8
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-10-30 22:51:24 -0600

    fix: use Effect bounds on effect composition functions

--- src/effect_composition.rs
+++ src/effect_composition.rs
@@ -1,32 +1,41 @@
 //! [`Effect`] composition functions, that can be used with [`and_compose`].
 //!
-//! [`Effect`]: crate::Effect
 //! [`and_compose`]: crate::system_combinators::and_compose
 
+use crate::Effect;
+
 /// [`Effect`] composition function that returns the first effect.
-///
-/// [`Effect`]: crate::Effect
-pub fn mandela<E0, E1>(e0: E0, _e1: E1) -> E0 {
+pub fn mandela<E0, E1>(e0: E0, _e1: E1) -> E0
+where
+    E0: Effect,
+    E1: Effect,
+{
     e0
 }
 
 /// [`Effect`] composition function that returns the second effect.
-///
-/// [`Effect`]: crate::Effect
-pub fn placebo<E0, E1>(_e0: E0, e1: E1) -> E1 {
+pub fn placebo<E0, E1>(_e0: E0, e1: E1) -> E1
+where
+    E0: Effect,
+    E1: Effect,
+{
     e1
 }
 
 /// [`Effect`] composition function that returns an effect that will apply both in order.
-///
-/// [`Effect`]: crate::Effect
-pub fn combine<E0, E1>(e0: E0, e1: E1) -> (E0, E1) {
+pub fn combine<E0, E1>(e0: E0, e1: E1) -> (E0, E1)
+where
+    E0: Effect,
+    E1: Effect,
+{
     (e0, e1)
 }
 
 /// [`Effect`] composition function that returns an effect that will apply both in reverse order.
-///
-/// [`Effect`]: crate::Effect
-pub fn enibmoc<E0, E1>(e0: E0, e1: E1) -> (E1, E0) {
+pub fn enibmoc<E0, E1>(e0: E0, e1: E1) -> (E1, E0)
+where
+    E0: Effect,
+    E1: Effect,
+{
     (e1, e0)
 }
