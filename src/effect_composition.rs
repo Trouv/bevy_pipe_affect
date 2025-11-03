a96c0e7e3371154ff30e6b66262c1e0c46dba8de
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-11-02 22:39:14 -0700

    feat: add more effect composition map-like functions, rename them, document them

--- src/effect_composition.rs
+++ src/effect_composition.rs
@@ -43,7 +43,22 @@ where
     (e1, e0)
 }
 
-pub fn only_if_some<E0, E1, E2>(
+/// Returns an [`Effect`] composition function that applies the given composition to the
+/// `Some`-wrapped left effect and the right effect, otherwise `None`.
+pub fn lhs_some_then<E0, E1, E2>(
+    composition: impl Fn(E0, E1) -> E2,
+) -> impl Fn(Option<E0>, E1) -> Option<E2>
+where
+    E0: Effect,
+    E1: Effect,
+    E2: Effect,
+{
+    move |option, e1| option.map(|e0| composition(e0, e1))
+}
+
+/// Returns an [`Effect`] composition function that applies the given composition to the left
+/// effect and the `Some`-wrapped right effect, otherwise `None`.
+pub fn rhs_some_then<E0, E1, E2>(
     composition: impl Fn(E0, E1) -> E2,
 ) -> impl Fn(E0, Option<E1>) -> Option<E2>
 where
@@ -51,10 +66,26 @@ where
     E1: Effect,
     E2: Effect,
 {
-    move |e0, e1| e1.map(|e1| composition(e0, e1))
+    move |e0, option| option.map(|e1| composition(e0, e1))
 }
 
-pub fn only_if_ok<E0, E1, E2, Er>(
+/// Returns an [`Effect`] composition function that applies the given composition to the
+/// `Ok`-wrapped left effect and the right effect, otherwise `Err`.
+pub fn lhs_ok_then<E0, E1, E2, Er>(
+    composition: impl Fn(E0, E1) -> E2,
+) -> impl Fn(Result<E0, Er>, E1) -> Result<E2, Er>
+where
+    E0: Effect,
+    E1: Effect,
+    E2: Effect,
+    Er: Into<BevyError>,
+{
+    move |result, e1| result.map(|e0| composition(e0, e1))
+}
+
+/// Returns an [`Effect`] composition function that applies the given composition to the left
+/// effect and the `Ok`-wrapped right effect, otherwise `Err`.
+pub fn rhs_ok_then<E0, E1, E2, Er>(
     composition: impl Fn(E0, E1) -> E2,
 ) -> impl Fn(E0, Result<E1, Er>) -> Result<E2, Er>
 where
@@ -63,10 +94,29 @@ where
     E2: Effect,
     Er: Into<BevyError>,
 {
-    move |e0, e1| e1.map(|e1| composition(e0, e1))
+    move |e0, result| result.map(|e1| composition(e0, e1))
 }
 
-pub fn only_if_handled<E0, E1, E2, Er, Handler>(
+/// Returns an [`Effect`] composition function that applies the given composition to the
+/// `AffectOrHandle { result: Ok, .. }`-wrapped left effect and the right effect, otherwise
+/// `AffectOrHandle { result: Err, .. }`.
+pub fn lhs_affect_then<E0, E1, E2, Er, Handler>(
+    composition: impl Fn(E0, E1) -> E2,
+) -> impl Fn(AffectOrHandle<E0, Er, Handler>, E1) -> AffectOrHandle<E2, Er, Handler>
+where
+    E0: Effect,
+    E1: Effect,
+    E2: Effect,
+    Er: Into<BevyError>,
+    Handler: FnOnce(BevyError, ErrorContext),
+{
+    move |affect_or_handle, e1| affect_or_handle.map(|e0| composition(e0, e1))
+}
+
+/// Returns an [`Effect`] composition function that applies the given composition to the left
+/// effect and the `AffectOrHandle { result: Ok, .. }`-wrapped right effect, otherwise `AffectOrHandle
+/// { result: Err, .. }`.
+pub fn rhs_affect_then<E0, E1, E2, Er, Handler>(
     composition: impl Fn(E0, E1) -> E2,
 ) -> impl Fn(E0, AffectOrHandle<E1, Er, Handler>) -> AffectOrHandle<E2, Er, Handler>
 where
@@ -76,8 +126,5 @@ where
     Er: Into<BevyError>,
     Handler: FnOnce(BevyError, ErrorContext),
 {
-    move |e0, AffectOrHandle { result, handler }| AffectOrHandle {
-        result: result.map(|e1| composition(e0, e1)),
-        handler,
-    }
+    move |e0, affect_or_handle| affect_or_handle.map(|e1| composition(e0, e1))
 }
