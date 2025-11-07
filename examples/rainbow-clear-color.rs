88cbad32205f848a6f23d77008bfe9456d709303
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-11-06 23:11:24 -0700

    docs: make rainbow-clear-color more basic, so it is the basic example for the repo

--- examples/rainbow-clear-color.rs
+++ examples/rainbow-clear-color.rs
@@ -4,34 +4,16 @@ use bevy_pipe_affect::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .init_resource::<NumUpdates>()
         .add_systems(
             Update,
-            sample_system_with_effect_and_output
-                .pipe(and_compose(
-                    sample_system_with_effect_and_input,
-                    |e1, e2| (e1, e2),
-                ))
+            pure(rainbow_clear_color) // pure() is optional, just forces the system to be read-only
                 .pipe(affect),
         )
         .run();
 }
 
-fn sample_system_with_effect_and_input(
-    In(theta): In<f32>,
-    current: Res<ClearColor>,
-) -> impl Effect {
-    res_set(ClearColor(current.0.rotate_hue(theta)))
-}
-
-#[derive(Resource, Default)]
-struct NumUpdates(u32);
-
-fn sample_system_with_effect_and_output(
-    num_updates: Res<NumUpdates>,
-) -> EffectOut<ResSet<NumUpdates>, f32> {
-    EffectOut {
-        effect: res_set(NumUpdates(num_updates.0 + 1)),
-        out: (num_updates.0 % 10) as f32 / 10.,
-    }
+/// In this system, the clear color is a pure function of time.
+fn rainbow_clear_color(time: Res<Time>) -> impl Effect {
+    let color = Color::hsv(time.elapsed_secs() * 20.0, 0.7, 0.7);
+    res_set(ClearColor(color))
 }
