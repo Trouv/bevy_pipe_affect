0733ecb8bd9a969f02f4a56db41cfeabd72313c1
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-11-06 22:51:25 -0700

    feat: add and_combine system combinator that is just and_compose with combine

--- src/system_combinators.rs
+++ src/system_combinators.rs
@@ -3,6 +3,7 @@
 use bevy::ecs::system::StaticSystemParam;
 use bevy::prelude::*;
 
+use crate::effect_composition::combine;
 use crate::{Effect, EffectOut};
 
 /// `bevy` system that accepts [`Effect`]s as pipe input and performs their state transition.
@@ -45,6 +46,9 @@ where
 /// Similarly, normal pipe output can be passed out of the resulting system if `S` returns
 /// [`EffectOut<E, O>`].
 ///
+/// Some basic effect composition functions are provided by this library in the
+/// [`effect_composition`] module.
+///
 /// In this example, the effect composition just ignores the effect of the original system:
 /// ```
 /// # use bevy::prelude::*;
@@ -67,6 +71,7 @@ where
 /// ```
 ///
 /// [`EffectOut<E, O>`]: EffectOut
+/// [`effect_composition`]: crate::effect_composition
 pub fn and_compose<I1, E1, O1, System, Marker, I2, E2, O2, E3>(
     mut s: System,
     compose_fn: impl Fn(E1, E2) -> E3,
@@ -92,6 +97,47 @@ where
             out,
         }
     }
+}
+
+/// Higher-order `bevy` system constructor for combining the effects of two systems via piping.
+///
+/// Accepts a system with effects and returns a system that combines the effects of the given
+/// system and the piped-in system.
+///
+/// This function is just [`and_compose`] used with [`combine`] for convenience. See [`and_compose`] for more
+/// effect composition flexibility.
+///
+/// ```
+/// # use bevy::prelude::*;
+/// # use bevy::ecs::system::assert_is_system;
+/// use bevy_pipe_affect::prelude::*;
+///
+/// fn system_with_effects() -> impl Effect {
+///     res_set(ClearColor(Color::BLACK))
+/// }
+///
+/// fn another_system_with_effects() -> impl Effect {
+///     res_set(UiScale(2.0))
+/// }
+///
+/// assert_is_system(
+///     system_with_effects
+///         .pipe(and_combine(another_system_with_effects))
+///         .pipe(affect), // applies both effects
+/// )
+/// ```
+pub fn and_combine<I1, E1, O1, System, Marker, I2, E2, O2>(
+    s: System,
+) -> impl FnMut(In<I1>, StaticSystemParam<System::Param>) -> EffectOut<(E1, E2), O2>
+where
+    System: SystemParamFunction<Marker, Out = I2>,
+    I1: Into<EffectOut<E1, O1>>,
+    I2: Into<EffectOut<E2, O2>>,
+    E1: Effect,
+    E2: Effect,
+    for<'a> System::In: SystemInput<Inner<'a> = O1>,
+{
+    and_compose(s, combine)
 }
 
 /// Identity function for read-only-systems.
