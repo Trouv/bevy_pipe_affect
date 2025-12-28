e54b4912f8d5a49e52d2fcb207606340a777fd88
Joda <jodainteractive@protonmail.com>
2025-12-27 20:41:22 -0600

    switch to run_system_once

--- examples/rainbow-clear-color.rs
+++ examples/rainbow-clear-color.rs
@@ -20,26 +20,18 @@ fn rainbow_clear_color(time: Res<Time>) -> ResSet<ClearColor> {
 
 #[cfg(test)]
 mod tests {
+    use bevy::ecs::system::RunSystemOnce;
 
-    use bevy::camera::ClearColor;
-    use bevy::color::Color;
-    use bevy::ecs::system::{Res, SystemState};
-    use bevy::ecs::world::World;
-    use bevy::time::{Real, Time};
-
-    use crate::rainbow_clear_color;
+    use super::*;
 
     #[test]
     fn test_rainbow_clear() {
-        // Create a Res<Time> for our system
+        // Create a World with a Resource for our Time
         let mut world = World::new();
         world.insert_resource::<Time>(Time::default());
 
-        let mut state: SystemState<(Res<Time>,)> = SystemState::new(&mut world);
-        let (time,) = state.get(&mut world);
-
         // Now that we have a Res<Time>, we can run our system and get an output
-        let output = rainbow_clear_color(time);
+        let output = world.run_system_once(rainbow_clear_color).unwrap();
 
         // Then all we have to do is check that output has the proper value
         assert_eq!(output.value.0, Color::hsva(0.0, 0.7, 0.7, 1.0));
