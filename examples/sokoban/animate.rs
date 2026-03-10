11156e5176331f7172f1d49ea54bd63b86ec8b99
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-09 22:18:40 -0600

    example: update animate_position system to use QueryMap

--- examples/sokoban/animate.rs
+++ examples/sokoban/animate.rs
@@ -10,17 +10,19 @@ const ANIMATION_DECAY_RATE: f32 = 50.;
 /// Defines how visual positions of entities update based off their logical position.
 pub fn animate_position(
     time: Res<Time>,
-) -> ComponentsSetWithQueryData<(Transform,), &'static Position> {
+) -> QueryMap<(&'static Transform, &'static Position), ComponentSet<Transform>> {
     let delta_secs = time.delta_secs();
-    components_set_with_query_data(move |(mut transform,): (Transform,), position: &Position| {
+    query_map(move |(transform, position): (&Transform, &Position)| {
         // note: even though we we use mut here, there is still no side effect to this closure.
         // This is because we take ownership of the transform instead of mutable reference.
         let target = (position.as_vec2() * BLOCK_SIZE).extend(0.0);
 
-        transform
+        let mut new_transform = *transform;
+
+        new_transform
             .translation
             .smooth_nudge(&target, ANIMATION_DECAY_RATE, delta_secs);
 
-        (transform,)
+        component_set(new_transform)
     })
 }
