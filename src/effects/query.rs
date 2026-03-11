60295882bc5b8d231b02f78df8be760824c47837
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-10 22:49:39 -0600

    feat: add QueryMapAnd effect

--- src/effects/query.rs
+++ src/effects/query.rs
@@ -3,8 +3,8 @@ use std::marker::PhantomData;
 use bevy::ecs::query::{QueryFilter, ReadOnlyQueryData};
 use bevy::prelude::*;
 
-use crate::Effect;
 use crate::query_data_effect::QueryDataEffect;
+use crate::{Effect, EffectOut, effect_out};
 
 pub struct QueryAffect<QueryDataE, Filter = ()>
 where
@@ -97,5 +97,77 @@ where
             .for_each(|(entity, query_data_effect)| {
                 query_data_effect.affect(&mut param.p1().get_mut(entity).expect("The entities in the first query are guaranteed to be a subset of the entities in the second query due to filters"));
             })
+    }
+}
+
+pub struct QueryMapAnd<QueryDataIn, E, QueryDataE, Filter = ()>
+where
+    QueryDataIn: ReadOnlyQueryData,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    Filter: QueryFilter,
+{
+    pub f: Box<dyn for<'w, 's> Fn(QueryDataIn::Item<'w, 's>) -> EffectOut<E, QueryDataE>>,
+    filter: PhantomData<Filter>,
+}
+
+pub fn query_map_and<QueryDataIn, E, QueryDataE, Filter, F>(
+    f: F,
+) -> QueryMapAnd<QueryDataIn, E, QueryDataE, Filter>
+where
+    QueryDataIn: ReadOnlyQueryData,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    Filter: QueryFilter,
+    F: for<'w, 's> Fn(QueryDataIn::Item<'w, 's>) -> EffectOut<E, QueryDataE> + 'static,
+{
+    QueryMapAnd {
+        f: Box::new(f),
+        filter: PhantomData,
+    }
+}
+
+impl<QueryDataIn, E, QueryDataE, Filter> Effect for QueryMapAnd<QueryDataIn, E, QueryDataE, Filter>
+where
+    QueryDataIn: ReadOnlyQueryData + 'static,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    QueryDataE::MutQueryData: 'static,
+    QueryDataE::Filter: 'static,
+    Filter: QueryFilter + 'static,
+{
+    type MutParam = ParamSet<
+        'static,
+        'static,
+        (
+            Query<'static, 'static, (Entity, QueryDataIn), (QueryDataE::Filter, Filter)>,
+            Query<'static, 'static, QueryDataE::MutQueryData, Filter>,
+            <Vec<E> as Effect>::MutParam,
+        ),
+    >;
+
+    fn affect(self, param: &mut <Self::MutParam as bevy::ecs::system::SystemParam>::Item<'_, '_>) {
+        let EffectOut {
+            effect: effects,
+            out: query_data_effects,
+        }: EffectOut<Vec<E>, Vec<(Entity, QueryDataE)>> = param
+            .p0()
+            .iter()
+            .map(|(entity, data_in)| {
+                let EffectOut {
+                    effect,
+                    out: query_data_effect,
+                } = (self.f)(data_in);
+                effect_out(effect, (entity, query_data_effect))
+            })
+            .collect();
+
+        query_data_effects
+            .into_iter()
+            .for_each(|(entity, query_data_effect)| {
+                query_data_effect.affect(&mut param.p1().get_mut(entity).expect("The entities in the first query are guaranteed to be a subset of the entities in the second query due to filters"));
+            });
+
+        effects.affect(&mut param.p2())
     }
 }
