32d7f6daf57c7a8a331acdded7ebc90c876b4b58
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-15 13:39:57 -0600

    feat: add QueryEntityMapAnd effect

--- src/effects/query_entity.rs
+++ src/effects/query_entity.rs
@@ -119,3 +119,78 @@ where
         query_data_effect.affect(&mut param.p1().get_mut(self.entity).expect("The entities in the first query are guaranteed to be a subset of the entities in the second query due to filters"));
     }
 }
+
+pub struct QueryEntityMapAnd<QueryDataIn, E, QueryDataE, Filter = ()>
+where
+    QueryDataIn: ReadOnlyQueryData,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    Filter: QueryFilter,
+{
+    pub entity: Entity,
+    pub f: Box<dyn for<'w, 's> Fn(QueryDataIn::Item<'w, 's>) -> EffectOut<E, QueryDataE>>,
+    filter: PhantomData<Filter>,
+}
+
+pub fn query_entity_map_and<QueryDataIn, E, QueryDataE, Filter, F>(
+    entity: Entity,
+    f: F,
+) -> QueryEntityMapAnd<QueryDataIn, E, QueryDataE, Filter>
+where
+    QueryDataIn: ReadOnlyQueryData,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    Filter: QueryFilter,
+    F: for<'w, 's> Fn(QueryDataIn::Item<'w, 's>) -> EffectOut<E, QueryDataE> + 'static,
+{
+    QueryEntityMapAnd {
+        entity,
+        f: Box::new(f),
+        filter: PhantomData,
+    }
+}
+
+impl<QueryDataIn, E, QueryDataE, Filter> Effect
+    for QueryEntityMapAnd<QueryDataIn, E, QueryDataE, Filter>
+where
+    QueryDataIn: ReadOnlyQueryData + 'static,
+    E: Effect,
+    QueryDataE: QueryDataEffect,
+    QueryDataE::MutQueryData: 'static,
+    QueryDataE::Filter: 'static,
+    Filter: QueryFilter + 'static,
+{
+    type MutParam = ParamSet<
+        'static,
+        'static,
+        (
+            (
+                Query<'static, 'static, QueryDataIn, (QueryDataE::Filter, Filter)>,
+                <Result<(), bevy::ecs::query::QueryEntityError> as Effect>::MutParam,
+            ),
+            Query<'static, 'static, QueryDataE::MutQueryData, Filter>,
+            <E as Effect>::MutParam,
+        ),
+    >;
+
+    fn affect(self, param: &mut <Self::MutParam as bevy::ecs::system::SystemParam>::Item<'_, '_>) {
+        let (input_query_param, mut result_param) = param.p0();
+
+        let input = match input_query_param.get(self.entity) {
+            Ok(d) => d,
+            Err(e) => {
+                Err::<(), _>(e).affect(&mut result_param);
+                return ();
+            }
+        };
+
+        let EffectOut {
+            effect,
+            out: query_data_effect,
+        } = (self.f)(input);
+
+        query_data_effect.affect(&mut param.p1().get_mut(self.entity).expect("The entities in the first query are guaranteed to be a subset of the entities in the second query due to filters"));
+
+        effect.affect(&mut param.p2())
+    }
+}
