077de1c1837079f6c45b60880d2571512fba84a8
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-31 21:41:46 -0600

    docs: add pure/impure examples for resource effects (#146)
    Comparing a pure system to the equivalent impure system is instructive,
    and their equivalence is a nice property to test. This change adds a
    pure/impure example and proptest to the resource effects.

--- src/effects/resource.rs
+++ src/effects/resource.rs
@@ -10,6 +10,55 @@ use crate::effect::Effect;
 /// [`Effect`] that sets a `Resource` to the provided value.
 ///
 /// Can be constructed by [`res_set`].
+///
+/// # Example
+/// In this example, a system is written that resets the `Score` to 0.
+/// ```
+/// use bevy::prelude::*;
+/// use bevy_pipe_affect::prelude::*;
+///
+/// #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Resource)]
+/// # #[derive(proptest_derive::Arbitrary)]
+/// struct Score(u32);
+///
+/// /// Pure system using effects.
+/// fn reset_score_pure() -> ResSet<Score> {
+///     res_set(Score(0))
+/// }
+///
+/// /// Equivalent impure system.
+/// fn reset_score_impure(mut score: ResMut<Score>) {
+///     score.0 = 0;
+/// }
+/// #
+/// # use proptest::prelude::*;
+/// #
+/// # fn app_setup(score: Score) -> App {
+/// #     let mut app = App::new();
+/// #     app.insert_resource(score);
+/// #     app
+/// # }
+/// #
+/// # fn resource_state(world: &World) -> &Score {
+/// #     world.get_resource::<Score>().unwrap()
+/// # }
+/// #
+/// # proptest! {
+/// #     fn main(score: Score) {
+/// #         let mut pure_app = app_setup(score);
+/// #         pure_app.add_systems(Update, reset_score_pure.pipe(affect));
+/// #
+/// #         let mut impure_app = app_setup(score);
+/// #         impure_app.add_systems(Update, reset_score_impure);
+/// #
+/// #         for _ in 0..3 {
+/// #              prop_assert_eq!(resource_state(pure_app.world_mut()), resource_state(impure_app.world_mut()));
+/// #              pure_app.update();
+/// #              impure_app.update();
+/// #         }
+/// #     }
+/// # }
+/// ```
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
 pub struct ResSet<R>
 where
@@ -38,9 +87,58 @@ where
     }
 }
 
-/// [`Effect`] that transforms a `Resource` with the provided function.
+/// [`Effect`] that transforms a `Resource` with the provided `R -> R` function.
 ///
 /// Can be constructed by [`res_set_with`].
+///
+/// # Example
+/// In this example, a system is written that increments the `Updates` by 1.
+/// ```
+/// use bevy::prelude::*;
+/// use bevy_pipe_affect::prelude::*;
+///
+/// #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Resource)]
+/// # #[derive(proptest_derive::Arbitrary)]
+/// struct Updates(u32);
+///
+/// /// Pure system using effects.
+/// fn increment_updates_pure() -> ResSetWith<Updates> {
+///     res_set_with(|Updates(n)| Updates(n + 1))
+/// }
+///
+/// /// Equivalent impure system.
+/// fn increment_updates_impure(mut updates: ResMut<Updates>) {
+///     updates.0 += 1;
+/// }
+/// #
+/// # use proptest::prelude::*;
+/// #
+/// # fn app_setup(updates: Updates) -> App {
+/// #     let mut app = App::new();
+/// #     app.insert_resource(updates);
+/// #     app
+/// # }
+/// #
+/// # fn resource_state(world: &World) -> &Updates {
+/// #     world.get_resource::<Updates>().unwrap()
+/// # }
+/// #
+/// # proptest! {
+/// #     fn main(updates: Updates) {
+/// #         let mut pure_app = app_setup(updates);
+/// #         pure_app.add_systems(Update, increment_updates_pure.pipe(affect));
+/// #
+/// #         let mut impure_app = app_setup(updates);
+/// #         impure_app.add_systems(Update, increment_updates_impure);
+/// #
+/// #         for _ in 0..20 {
+/// #              prop_assert_eq!(resource_state(pure_app.world_mut()), resource_state(impure_app.world_mut()));
+/// #              pure_app.update();
+/// #              impure_app.update();
+/// #         }
+/// #     }
+/// # }
+/// ```
 #[derive(derive_more::Debug)]
 pub struct ResSetWith<R>
 where
